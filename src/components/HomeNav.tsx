8f6af59f0ffa0b70b132593c8ff18be555c6676c
Benedict Gabriel <benedictgabriel73@gmail.com>
2024-06-17 17:44:08 +0100

    Added menu to small screen

--- src/components/HomeNav.tsx
+++ src/components/HomeNav.tsx
@@ -7,17 +7,18 @@ import { AiOutlineCompass } from "react-icons/ai";
 import { GrLocation } from "react-icons/gr";
 import { HiOutlineUserGroup } from "react-icons/hi2";
 import { IoPersonOutline } from "react-icons/io5";
+import { SlMenu } from "react-icons/sl";
+import { RiCloseLargeFill } from "react-icons/ri";
+import { useState } from "react";
 
 const NavItem = ({
   to = "#",
   text = "",
   Icon,
-  isActive = false,
 }: {
   to?: string;
   text: string;
   Icon: IconType;
-  isActive?: boolean;
 }) => {
   return (
     <NavLink
@@ -37,6 +38,8 @@ const NavItem = ({
 };
 
 const HomeNav = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <div className="w-full gap-4 flex flex-col sticky top-0 z-10 bg-white h-48 pt-8">
       <div className="flex flex-row flex-wrap w-full items-center gap-4">
@@ -46,9 +49,12 @@ const HomeNav = () => {
         <div className="flex justify-center flex-[100%] sm:flex-1 order-3 sm:order-2">
           <Search />
         </div>
-        <div className="w-24 flex justify-end ml-auto order-2 sm:order-3">
-          <button className="mt-4">
-            <GoBell className="text-xl text-gray-500" />
+        <div className="w-24 flex justify-end items-center ml-auto order-2 sm:order-3 gap-4 mt-4 text-xl text-gray-500">
+          <button className="">
+            <GoBell className="" />
+          </button>
+          <button className="flex sm:hidden" onClick={() => setOpenMenu(true)}>
+            <SlMenu />
           </button>
         </div>
       </div>
@@ -58,6 +64,35 @@ const HomeNav = () => {
           <NavItem text="Trips" Icon={GrLocation} to="/trips" />
           <NavItem text="Crew" Icon={HiOutlineUserGroup} to="/crew" />
           <NavItem text="Profile" Icon={IoPersonOutline} to="/profile" />
+        </div>
+      </div>
+      <div
+        className={`fixed top-0 bottom-0 w-full z-20 bg-black bg-opacity-10 sm:hidden flex ${
+          openMenu ? "right-[0%]" : "right-[100%]"
+        } transition-all duration-300`}
+        onClick={() => setOpenMenu(false)}
+      >
+        <div
+          className={`flex flex-col bg-white h-full w-full max-w-xs p-4 ${
+            openMenu ? "right-[0%]" : "right-[100%]"
+          } transition-all duration-500`}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="mt-4 mb-8 flex justify-between">
+            <Logo style="ml-4" />
+            <button
+              onClick={() => setOpenMenu(false)}
+              className="text-gray-500 text-xl"
+            >
+              <RiCloseLargeFill />
+            </button>
+          </div>
+          <div className="flex flex-col gap-4">
+            <NavItem text="Explore" Icon={AiOutlineCompass} to="/explore" />
+            <NavItem text="Trips" Icon={GrLocation} to="/trips" />
+            <NavItem text="Crew" Icon={HiOutlineUserGroup} to="/crew" />
+            <NavItem text="Profile" Icon={IoPersonOutline} to="/profile" />
+          </div>
         </div>
       </div>
     </div>
